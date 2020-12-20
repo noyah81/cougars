361bc31b1ba0df37e0a6c6462d35b5d94fb80837
Calvin Ni <61429026+statsprojectlol@users.noreply.github.com>
2020-12-19 17:00:34 -0800

    Added app route template

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route("/")
+def ():
+  return render_template(".html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1',port='3000')
