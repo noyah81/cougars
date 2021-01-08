a5f454b91e22fb29340e428d5b3afd18968ae2e4
noyah81 <59671860+noyah81@users.noreply.github.com>
2021-01-08 01:14:59 -0800

    home

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def root():
   return render_template("base.html")
 
+@app.route("/madisonsquare")
+def madisonsquare():
+    return render_template("madisonsquare.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1',port='3000')
