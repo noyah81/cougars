710422b9cac5bc7c0eb08862d4dec61fd27e9a79
noyah81 <59671860+noyah81@users.noreply.github.com>
2021-01-14 22:04:22 -0800

    nav bar and routes, interest point files

--- main.py
+++ main.py
@@ -1,22 +1,57 @@
 from flask import Flask, jsonify, render_template, request
-import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def root():
-  return render_template("base.html")
+    return render_template("base.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 
 @app.route("/madisonsquare")
 def madisonsquare():
     return render_template("madisonsquare.html")
 
+
+@app.route("/liberty")
+def liberty():
+    return render_template("liberty.html")
+
+@app.route("/time")
+def time():
+    return render_template("time.html")
+
+@app.route("/central")
+def central():
+    return render_template("central.html")
+
+
+@app.route("/museum")
+def museum():
+    return render_template("museum.html")
+
+
+@app.route("/statue")
+def statue():
+    return render_template("statue.html")
+
+
+@app.route("/empire")
+def empire():
+    return render_template("empire.html")
+
+
 @app.route("/Wallstreetstation")
 def Wallstreetstation():
     return render_template("Wallstreetstation.html")
 
+
 # connects default URL to a function
-@app.route('/joke',  methods=['GET', 'POST'])
+@app.route('/joke', methods=['GET', 'POST'])
 def joke():
     # call to random joke web api
     url = 'https://official-joke-api.appspot.com/jokes/programming/random'
@@ -25,6 +60,7 @@ def joke():
     setup = resp.json()[0]['setup']
     punchline = resp.json()[0]['punchline']
     return render_template("joke.html", setup=setup, punchline=punchline)
+
 
 @app.route('/stop')
 def station():
@@ -39,6 +75,7 @@ def station():
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
 
+
 # Given station, returns all info about that station
 @app.route('/api')
 def api():
@@ -50,13 +87,13 @@ def api():
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
     except KeyError:
-        print ('Invalid Key')
+        print('Invalid Key')
         response = jsonify(result='key not found')
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
 
 
-#Given time, returns all stations where train stops at that time
+# Given time, returns all stations where train stops at that time
 @app.route('/times')
 def times():
     if len(request.args) != 2:
@@ -74,11 +111,13 @@ def times():
             for arrival in arrivals:
                 a = arrival.split(":")
                 if a[0] == hour and a[1] == minute:
-                    result = {'id': key, 'name': data[key]['name'], 'lat': data[key]['lat'], 'lon': data[key]['lon'], 'arrival': arrival}
+                    result = {'id': key, 'name': data[key]['name'], 'lat': data[key]['lat'], 'lon': data[key]['lon'],
+                              'arrival': arrival}
                     results.append(result)
         response = jsonify(result=results)
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
+
 
 # Provide ids for all stations
 @app.route('/stations')
@@ -91,5 +130,6 @@ def stations():
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1',port='3000')
+    app.run(debug=True, host='127.0.0.1', port='5000')
