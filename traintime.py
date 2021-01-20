25125da69e25285ed0bbae1adce33e78cb08237f
Calvin Ni <61429026+statsprojectlol@users.noreply.github.com>
2021-01-20 14:09:35 -0800

    made the database prototype

--- traintime.py
+++ traintime.py
@@ -1,6 +1,6 @@
 def TT1():
     time = "Every 5 mins"
-    line = "Red 1"
+    line = "Blue A"
     info = {"time": time, "line":line}
     return info
 
@@ -28,5 +28,89 @@ def TT5():
     info = {"time": time, "line":line}
     return info
 
+def TT6():
+    time = "Every 5 mins"
+    line = "Green 6 "
+    info = {"time": time, "line":line}
+    return info
+
+def TT7():
+    time = "Every 5 mins"
+    line = "Purple 7 "
+    info = {"time": time, "line":line}
+    return info
+
+def TT8():
+    time = "Every 5 mins"
+    line = "Blue A "
+    info = {"time": time, "line":line}
+    return info
+
+def TT9():
+    time = "Every 5 mins"
+    line = "Blue C "
+    info = {"time": time, "line":line}
+    return info
+def TT10():
+    time = "Every 5 mins"
+    line = "Orange D "
+    info = {"time": time, "line":line}
+    return info
+
+def TT11():
+    time = "Every 5 mins"
+    line = "Blue E "
+    info = {"time": time, "line":line}
+    return info
+def TT12():
+    time = "Every 5 mins"
+    line = "Orange F "
+    info = {"time": time, "line":line}
+    return info
+def TT13():
+    time = "Every 5 mins"
+    line = "Grey SF "
+    info = {"time": time, "line":line}
+    return info
+
+def TT14():
+    time = "Every 5 mins"
+    line = "Light-green G "
+    info = {"time": time, "line":line}
+    return info
+
+def WT1():
+    time = "Every 5 mins"
+    line = "Grey SR "
+    info = {"time": time, "line":line}
+    return info
+
+def WT2():
+    time = "Every 5 mins"
+    line = "Brown J "
+    info = {"time": time, "line":line}
+    return info
+
+def WT3():
+    time = "Every 5 mins"
+    line = "Light-grey L "
+    info = {"time": time, "line":line}
+    return info
+
+def WT4():
+    time = "Every 5 mins"
+    line = "Green 5 "
+    info = {"time": time, "line":line}
+    return info
+
+def WT5():
+    time = "Every 5 mins"
+    line = "Green 5 "
+    info = {"time": time, "line":line}
+    return info
+
+
+
+
 def TTT():
-    return [TT1(),TT2(),TT3(),TT4(),TT5()]
+    return [TT1(),TT2(),TT3(),TT4(),TT5(),TT6(),TT7(),TT8(),TT9(),TT10(),TT11(),TT12(),TT13(),TT14(),TT15(),TT16(),TT17(),TT18(),TT19()]
