89d878a1d61be09adb19e00c78fd95a7502ed93b
Calvin Ni <61429026+statsprojectlol@users.noreply.github.com>
2021-01-20 22:19:29 -0800

    Prototype API done

---
+++ restapi.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from restapi import restapi_bp
+from models.lessons import menus
+import requests
+
+# connects default URL to a function
+@restapi_bp.route('/')
+def index():
+    return render_template("restapi/index.html", menus=menus)
+
+@restapi_bp.route('/stations',  methods=['GET', 'POST'])
+def joke():
+    # call to random joke web api
+    url = 'mtaapi.herokuapp.com/stations'
+    response = requests.get(url)
+    # formatting variables from return
+    time = response.json()[0]['id']
+    name = response.json()[0]['name']
+    return render_template("restapi/stations.html", menus=menus,  time=time, name=name)
+
